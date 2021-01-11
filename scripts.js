efba53ec3d1d09292b8ed2b7667c759a67cee048
Aditya Bose <13054902+adbose@users.noreply.github.com>
2021-01-11 18:56:02 +0530

    Fix form validation

--- scripts.js
+++ scripts.js
@@ -9,40 +9,47 @@ form.addEventListener('submit', e => {
 	console.log('sumbit event triggered');
 	const phoneNumberValue = phoneNumber.value.trim();
 	const messageValue = message.value.trim();
+	if(messageValue){
+		showValid(message);
+	}
 	const formattedNumber = formatNumber(phoneNumberValue);
 	console.log(formattedNumber);
 
-	if (formattedNumber) {
+	if(formattedNumber){
 		generateLink(formattedNumber, messageValue);
 	}
 });
 
-function formatNumber(phoneNumberValue) {
-	if(phoneNumberValue === '') {
+function formatNumber(phoneNumberValue){
+	if(phoneNumberValue === ''){
 		setErrorFor(phoneNumber, 'Mobile number cannot be blank');
-	} else if (phoneNumberValue.startsWith('+')) {
+	} else if(phoneNumberValue.startsWith('+')){
 		setErrorFor(phoneNumber, 'Mobile number cannot start with +');
-	} else if (isNaN(phoneNumberValue)) {
+	} else if(isNaN(phoneNumberValue)){
 		setErrorFor(phoneNumber, 'Mobile number invalid')
-	} else if (!isNaN(phoneNumberValue)) {
+	} else if(!isNaN(phoneNumberValue)){
+		showValid(phoneNumber);
 		return phoneNumberValue;
 	}
 	return null;
-	
 }
 
-function setErrorFor(input, msg) {
+function showValid(input){
+	const formControl = input.parentElement;
+	formControl.className = 'form-control success';
+}
+
+function setErrorFor(input, msg){
 	const formControl = input.parentElement;
 	const small = formControl.querySelector('small');
 	formControl.className = 'form-control error';
 	small.innerText = msg;
 }
 
-function generateLink(number, message) {
+function generateLink(number, message){
 	let url = "https://wa.me/";
 	let endUrl = `${url}${number}?text=${encodeURIComponent(message)}`;
 	document.getElementById('link').innerText = endUrl;
 	document.getElementById('msg').innerText = 'Share the link below to let others message you on WhatsApp';
-
 }
 
